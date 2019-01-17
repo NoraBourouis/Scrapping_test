e2ca30d3c5717bf0c7dc10263708b8e4ec3bfe40
 NoraBourouis <bourouisnora@gmail.com>
2019-01-17 15:41:21 +0100

    hashes dans un array motherfuckeeer

--- lib/trader.rb
+++ lib/trader.rb
@@ -2,24 +2,35 @@ require 'nokogiri'
 require 'open-uri'
 
 
+
 page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+
 
 keys = page.xpath('//*/td[3]') 
 values = page.xpath('//*/td[5]/a')
 
 keyz = []
+valeur = []
+
 keys.each do |key|
   #puts key.text #ou n'importe quelle autre opération de ton choix ;)
   keyz << key.text
 end
 
-valeur = []
 values.each do |value|
     #puts value.text
     valeur << value.text
 end
 
+
 crypto = Hash[keyz.zip(valeur.map)]
+array_cryp = []
 
-puts crypto
-
+crypto.each { |k,v| v[0] = ""}
+crypto.transform_values!(&:to_f)
+crypto.each do |key, value|
+    hash = Hash.new
+        hash[key] = value
+        array_cryp << hash
+    end
+puts array_cryp
