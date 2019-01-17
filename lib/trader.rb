b5666a6afb2bcef0a294f051448d09a5c8148aff
 NoraBourouis <bourouisnora@gmail.com>
2019-01-17 13:04:17 +0100

    exo 1 terminé

--- lib/trader.rb
+++ lib/trader.rb
@@ -1,2 +1,25 @@
 require 'nokogiri'
 require 'open-uri'
+
+
+page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+
+keys = page.xpath('//*/td[3]') 
+values = page.xpath('//*/td[5]/a')
+
+keyz = []
+keys.each do |key|
+  #puts key.text #ou n'importe quelle autre opération de ton choix ;)
+  keyz << key.text
+end
+
+valeur = []
+values.each do |value|
+    #puts value.text
+    valeur << value.text
+end
+
+crypto = Hash[keyz.zip(valeur.map)]
+
+puts crypto 
+
